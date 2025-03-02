48c3894aaf68073ef1904d40a9394a8ea09d419d
MaiNgocHoangLong-edu <125291302+Maingochoanglong@users.noreply.github.com>
2025-03-02 21:48:45 +0700

    Ex5_AddSubMulDiv_Var

---
+++ Ex5_AddSubMulDiv_Var/app/src/main/java/ntu/maingochoanglong/ex5_addsubmuldiv_var/MainActivity.java
@@ -0,0 +1,101 @@
+package ntu.maingochoanglong.ex5_addsubmuldiv_var;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        TimDieuKhien();
+        button_Cong.setOnClickListener(boLangNghe_XuLyCong);
+        button_Tru.setOnClickListener(boLangNghe_XuLyTru);
+        button_Nhan.setOnClickListener(boLangNghe_XuLyNhan);
+        button_Chia.setOnClickListener(boLangNghe_XuLyChia);
+    }
+
+    View.OnClickListener boLangNghe_XuLyCong = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            XuLyCong();
+        }
+    };
+
+    View.OnClickListener boLangNghe_XuLyTru = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            XuLyTru();
+        }
+    };
+
+    View.OnClickListener boLangNghe_XuLyNhan = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            XuLyNhan();
+        }
+    };
+
+    View.OnClickListener boLangNghe_XuLyChia = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            XuLyChia();
+        }
+    };
+
+    void XuLyCong() {
+        float soThuNhat = Float.parseFloat(editText_soThuNhat.getText().toString());
+        float soThuHai = Float.parseFloat(editText_soThuHai.getText().toString());
+        float ketQua = soThuNhat + soThuHai;
+        editText_ketQua.setText(String.valueOf(ketQua));
+    }
+
+    void XuLyTru() {
+        float soThuNhat = Float.parseFloat(editText_soThuNhat.getText().toString());
+        float soThuHai = Float.parseFloat(editText_soThuHai.getText().toString());
+        float ketQua = soThuNhat - soThuHai;
+        editText_ketQua.setText(String.valueOf(ketQua));
+    }
+
+    void XuLyNhan() {
+        float soThuNhat = Float.parseFloat(editText_soThuNhat.getText().toString());
+        float soThuHai = Float.parseFloat(editText_soThuHai.getText().toString());
+        float ketQua = soThuNhat * soThuHai;
+        editText_ketQua.setText(String.valueOf(ketQua));
+    }
+
+    void XuLyChia() {
+        float soThuNhat = Float.parseFloat(editText_soThuNhat.getText().toString());
+        float soThuHai = Float.parseFloat(editText_soThuHai.getText().toString());
+        float ketQua = soThuNhat / soThuHai;
+        editText_ketQua.setText(String.valueOf(ketQua));
+    }
+
+    void TimDieuKhien() {
+        editText_soThuNhat = findViewById(R.id.editText_soThuNhat);
+        editText_soThuHai = findViewById(R.id.editText_soThuHai);
+        editText_ketQua = findViewById(R.id.editText_ketQua);
+        button_Cong = findViewById(R.id.button_Cong);
+        button_Tru = findViewById(R.id.button_Tru);
+        button_Nhan = findViewById(R.id.button_Nhan);
+        button_Chia = findViewById(R.id.button_Chia);
+    }
+
+    EditText editText_soThuNhat, editText_soThuHai, editText_ketQua;
+    Button button_Cong, button_Tru, button_Nhan, button_Chia;
+}
