4be35534a1f978a99517a4c797247ccd08f29f7f
MaiNgocHoangLong-edu <125291302+Maingochoanglong@users.noreply.github.com>
2025-03-09 01:45:52 +0700

    Cập nhật

--- Ex7_IntentLogin/app/src/main/java/ntu/maingochoanglong/ex7_intentlogin/HomeActivity.java
+++ Ex7_IntentLogin/app/src/main/java/ntu/maingochoanglong/ex7_intentlogin/HomeActivity.java
@@ -1,12 +1,18 @@
 package ntu.maingochoanglong.ex7_intentlogin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import org.w3c.dom.Text;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -20,5 +26,12 @@ public class HomeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Intent iQuiz = getIntent();
+        String userName = iQuiz.getStringExtra("user_name");
+
+        TextView tvUserName = findViewById(R.id.tvUserName);
+        tvUserName.setText(userName);
+        tvUserName.setTextSize(TypedValue.COMPLEX_UNIT_SP,24);
     }
 }
--- Ex7_IntentLogin/app/src/main/java/ntu/maingochoanglong/ex7_intentlogin/LoginActivity.java
+++ Ex7_IntentLogin/app/src/main/java/ntu/maingochoanglong/ex7_intentlogin/LoginActivity.java
@@ -1,6 +1,11 @@
 package ntu.maingochoanglong.ex7_intentlogin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +24,30 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btnOK = findViewById(R.id.btnOK);
+
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText edtUserName = findViewById(R.id.edtUserName);
+                EditText edtPass = findViewById(R.id.edtPass);
+                EditText edtMail = findViewById(R.id.edtMail);
+
+                String userName = edtUserName.getText().toString();
+                String pass = edtPass.getText().toString();
+                String mail = edtMail.getText().toString();
+
+                if (userName.equals("mai ngoc hoang long") && pass.equals("ABC123") && mail.equals("ABC@gmail.com")) {
+                    Intent iQuiz = new Intent(LoginActivity.this, HomeActivity.class);
+                    iQuiz.putExtra("user_name",userName);
+                    startActivity(iQuiz);
+                }
+                else {
+                    Toast.makeText(LoginActivity.this, "BẠN NHẬP SAI THÔNG TIN!", Toast.LENGTH_LONG).show();
+                }
+            }
         });
     }
 }
