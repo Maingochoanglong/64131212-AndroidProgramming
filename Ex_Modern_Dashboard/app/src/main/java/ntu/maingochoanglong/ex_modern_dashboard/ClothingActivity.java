c3db58a6638c2b9d4dd4f65d6ef47def3cc44277
MaiNgocHoangLong-edu <125291302+Maingochoanglong@users.noreply.github.com>
2025-03-15 02:12:42 +0700

    dashboard

--- Ex_Modern_Dashboard/app/src/main/java/ntu/maingochoanglong/ex_modern_dashboard/ClothingActivity.java
+++ Ex_Modern_Dashboard/app/src/main/java/ntu/maingochoanglong/ex_modern_dashboard/ClothingActivity.java
@@ -1,39 +1,24 @@
 package ntu.maingochoanglong.ex_modern_dashboard;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    CardView clothingCard;
+public class ClothingActivity extends AppCompatActivity {
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_clothing);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        clothingCard = findViewById(R.id.clothingCard);
-        clothingCard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ClothingActivity.class);
-                startActivity(intent);
-            }
         });
     }
 }
