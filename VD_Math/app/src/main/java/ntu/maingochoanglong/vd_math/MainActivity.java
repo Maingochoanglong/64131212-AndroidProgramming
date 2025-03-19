904059b7bcf3fd0a5f812033f136a687b3eefb51
MaiNgocHoangLong-edu <125291302+Maingochoanglong@users.noreply.github.com>
2025-03-20 03:06:40 +0700

    Xử lí nút số

--- VD_Math/app/src/main/java/ntu/maingochoanglong/vd_math/MainActivity.java
+++ VD_Math/app/src/main/java/ntu/maingochoanglong/vd_math/MainActivity.java
@@ -1,6 +1,9 @@
 package ntu.maingochoanglong.vd_math;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    List<Button> button_num = new ArrayList<Button>();
+    Button button_Check, button_Delete, button_Restart;
+    EditText editText_NumA, editText_NumB, editText_KetQua;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +31,41 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        TimDieuKien();
+        XuLyNutSo();
+    }
+
+    void XuLyNutSo() {
+        for (var button : button_num) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (editText_KetQua.getText().toString().isEmpty()) {
+                        editText_KetQua.setText(String.valueOf(button_num.indexOf(button)));
+                    } else {
+                        editText_KetQua.setText(editText_KetQua.getText().append(String.valueOf(button_num.indexOf(button))));
+                    }
+                }
+            });
+        }
+    }
+
+    void TimDieuKien() {
+        button_num.add(findViewById(R.id.button_0));
+        button_num.add(findViewById(R.id.button_1));
+        button_num.add(findViewById(R.id.button_2));
+        button_num.add(findViewById(R.id.button_3));
+        button_num.add(findViewById(R.id.button_4));
+        button_num.add(findViewById(R.id.button_5));
+        button_num.add(findViewById(R.id.button_6));
+        button_num.add(findViewById(R.id.button_7));
+        button_num.add(findViewById(R.id.button_8));
+        button_num.add(findViewById(R.id.button_9));
+        button_Check = findViewById(R.id.button_Check);
+        button_Delete = findViewById(R.id.button_Delete);
+        button_Restart = findViewById(R.id.button_Restart);
+        editText_NumA = findViewById(R.id.editText_NumA);
+        editText_NumB = findViewById(R.id.editText_NumB);
+        editText_KetQua = findViewById(R.id.editText_KetQua);
     }
 }
