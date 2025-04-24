ca76b6317430195006489e18e825b89ecb6e79a1
Mai Ngọc Hoàng Long <125291302+Maingochoanglong@users.noreply.github.com>
2025-04-25 01:20:42 +0700

    Navigation View

--- NavigationView/app/src/main/java/ntu/maingochoanglong/navigationview/MainActivity.java
+++ NavigationView/app/src/main/java/ntu/maingochoanglong/navigationview/MainActivity.java
@@ -1,12 +1,19 @@
 package ntu.maingochoanglong.navigationview;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +26,26 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                Fragment selectedFragment = null;
+                if (id == R.id.menu_home) {
+                    selectedFragment = new HomeFragment();
+                } else if (id == R.id.menu_search) {
+                    selectedFragment = new SearchFragment();
+                } else if (id == R.id.menu_profile) {
+                    selectedFragment = new ProfileFragment();
+                }
+                if (selectedFragment != null)
+                    fragmentManager.beginTransaction().replace(R.id.fragmentContainerView, selectedFragment).commit();
+                return true;
+            }
         });
     }
 }
