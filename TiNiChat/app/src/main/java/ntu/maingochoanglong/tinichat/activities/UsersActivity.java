00114f903dc133172bc15e2cb5c27196602cae97
Mai Ngọc Hoàng Long <125291302+Maingochoanglong@users.noreply.github.com>
2025-05-19 03:22:50 +0700

    Display User List From Firestore Database

---
+++ TiNiChat/app/src/main/java/ntu/maingochoanglong/tinichat/activities/UsersActivity.java
@@ -0,0 +1,87 @@
+package ntu.maingochoanglong.tinichat.activities;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ntu.maingochoanglong.tinichat.adapters.UsersAdapter;
+import ntu.maingochoanglong.tinichat.databinding.ActivityUsersBinding;
+import ntu.maingochoanglong.tinichat.models.User;
+import ntu.maingochoanglong.tinichat.utilities.Constants;
+import ntu.maingochoanglong.tinichat.utilities.PreferenceManager;
+
+public class UsersActivity extends AppCompatActivity {
+
+    private ActivityUsersBinding binding;
+    private PreferenceManager preferenceManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityUsersBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        setListeners();
+        getUser();
+    }
+
+    private void setListeners() {
+        binding.imageBack.setOnClickListener(v -> getOnBackPressedDispatcher().onBackPressed());
+    }
+
+    private void getUser() {
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .get()
+                .addOnCompleteListener(task -> {
+                    loading(false);
+                    String currentUserId = preferenceManager.getString(Constants.KEY_USER_ID);
+                    if (task.isSuccessful() && task.getResult() != null) {
+                        List<User> users = new ArrayList<>();
+                        for (QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()) {
+                            if (currentUserId.equals(queryDocumentSnapshot.getId())) {
+                                continue;
+                            }
+                            User user = new User();
+                            user.name = queryDocumentSnapshot.getString(Constants.KEY_NAME);
+                            user.email = queryDocumentSnapshot.getString(Constants.KEY_EMAIL);
+                            user.image = queryDocumentSnapshot.getString(Constants.KEY_IMAGE);
+                            user.token = queryDocumentSnapshot.getString(Constants.KEY_FCM_TOKEN);
+                            users.add(user);
+                        }
+                        if (!users.isEmpty()) {
+                            UsersAdapter usersAdapter = new UsersAdapter(users);
+                            binding.usersRecyclerView.setAdapter(usersAdapter);
+                            binding.usersRecyclerView.setVisibility(View.VISIBLE);
+                        } else {
+                            showErrorMessage();
+                        }
+                    } else {
+                        showErrorMessage();
+                    }
+                });
+    }
+
+    private void showErrorMessage() {
+        binding.textErrorMessage.setText(String.format("%s","No user available"));
+        binding.textErrorMessage.setVisibility(View.VISIBLE);
+    }
+
+    private void loading(Boolean isLoading) {
+        if (isLoading) {
+            binding.progressBar.setVisibility(View.VISIBLE);
+        } else {
+            binding.progressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+}
---
+++ TiNiChat/app/src/main/java/ntu/maingochoanglong/tinichat/models/User.java
@@ -0,0 +1,7 @@
+package ntu.maingochoanglong.tinichat.models;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    public String name, image, email, token;
+}
